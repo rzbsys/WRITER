1bded9c700d98c10d3829c778eec50e608718923
rzbsys <20-10520@dshs.kr>
2021-12-25 21:54:56 +0900

    Pharse > Nouns

--- tfidf.py
+++ tfidf.py
@@ -2,7 +2,7 @@ from konlpy.tag import Okt
 okt = Okt()
 
 def keyword_extractor(text):
-    tokens = okt.phrases(text)
+    tokens = okt.nouns(text)
     tokens = [ token for token in tokens if len(token) > 1 ] # 한 글자인 단어는 제외
     count_dict = [(token, text.count(token)) for token in tokens ]
     ranked_words = sorted(count_dict, key=lambda x:x[1], reverse=True)[:10]
